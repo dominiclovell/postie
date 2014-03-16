d91868df21c3629354e5ffca9a08d82eeadd4eb2
Dominic Lovell <dom@domlovell.com>
2014-03-16 18:38:59 +1100

    Update methods for newer gems support

--- lib/postie.rb
+++ lib/postie.rb
@@ -13,7 +13,6 @@ module Postie
       DataMapper.setup(:postie, "sqlite3://#{File.expand_path(File.join(File.dirname(__FILE__), '..', 'db', 'postcodes.db'))}")
     end
     
-    mime :json, "application/json"
     before { content_type :json }
     
     get '/postcode' do
